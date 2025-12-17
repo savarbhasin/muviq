f44c81cf1a32c252aa9f80f44691ffc52bf152d1
S <51899822+savarbhasin@users.noreply.github.com>
2025-12-17 23:28:17 +0530

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -7,9 +7,7 @@ declare global {
 
 export const db =
   globalThis.prisma ??
-  new PrismaClient({
-    datasourceUrl: process.env.DATABASE_URL,
-  });
+  new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
   globalThis.prisma = db;
